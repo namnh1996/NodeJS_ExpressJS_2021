2d9229dea8990751226b71fadf8d0289ebf5c2da
unknown <hainam12101996@gmail.com>
2021-03-26 22:08:16 +0700

    success_view_user

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const shortid = require('shortid');
 const port = 3000;
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
 app.set('view engine','pug');
 app.set('views', './views');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
- 
-const adapter = new FileSync('db.json')
-const db = low(adapter)
 //set defaults db
 // db.defaults({users: []}).write();
 let users = db.get('users').value();
@@ -44,12 +45,22 @@ app.get('/users/create', function(req,res){
     res.render('users/create');
 })
 
+app.get('/users/:id', function(req,res){
+    let id = req.params.id;
+    let user = db.get('users').find({id:id}).value();
+    res.render('users/view',{
+        user: user
+    }); 
+})
+
 app.post('/users/create',function(req,res){
     //console.log(req.body);
     //users.push(req.body);
+    req.body.id = shortid.generate();
     db.get('users').push(req.body).write()
     res.redirect('/users')
 })
+
 
 app.listen(3000, function(){
     console.log('Server listening on port ' + port);
