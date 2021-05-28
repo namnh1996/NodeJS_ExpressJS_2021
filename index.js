1c1093b4c825460ed201948bd9961a8ac04b2d38
HAINAM-7 <user@users-MacBook-Pro.local>
2021-05-29 00:34:07 +0700

    fix bodyParser to heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser', { useNewUrlParser: true });
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL);
 
